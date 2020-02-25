e2adbd46d5e0e2b84cd9d71b8aaa074d4380310e
Geoffrey <geoffreylevenez@gmail.com>
2020-02-25 08:31:19 +0100

    Feat (admin) Erreur de commit, j'ai effacé mon travail -_-, début de nettoyage

--- app/src/Controller/BlogController.php
+++ app/src/Controller/BlogController.php
@@ -3,11 +3,17 @@
 namespace App\Controller;
 
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\MaPremiereTable;
-use App\Repository\MaPremiereTableRepository;
+use App\Repository\ArticleRepository;
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Form\ArticleType;
 
 
 
@@ -15,19 +21,7 @@ use App\Repository\MaPremiereTableRepository;
 
 class BlogController extends AbstractController
 {
-    /*
-     * @Route("/blog", name="blog")
-     */
-    /* public function list()
-      {
-      $repo = $this->getDoctrine()->getRepository( MaPremiereTable::class );
-      $articles = $repo->findAll();
-      return $this->render( "blog/default.html.twig", [
-      "articles" => $articles
-      ] );
-      } */
-
-    /**
+   /**
      *
      * @Route("/", name="home")
      */
@@ -37,10 +31,10 @@ class BlogController extends AbstractController
     }
 
     /**
-     * Cette m�thode est �gale � la m�thode pr�c�dente mais simplifie la syntaxe de l'injection de d�pendance du repository
+     * 
      * @Route("/blog", name="blog")
      */
-    public function list( MaPremiereTableRepository $repo )
+    public function list( ArticleRepository $repo )
     {
         $articles = $repo->findAll();
         return $this->render( "blog/articleList.html.twig", [
@@ -51,16 +45,43 @@ class BlogController extends AbstractController
     /**
      *
      * @Route("/blog/CREATE", name="blog_create")
+     * @Route("/blog/{id}/UPDATE", name="blog_update")
      */
-    public function create()
+    public function form( Article $article = null,  Request $request, EntityManagerInterface $manager )
     {
-        return $this->render( "blog/create.html.twig" );
+        if(!$article)
+        {
+            $article = new Article();
+        }
+        $form = $this->createForm(ArticleType::class, $article);
+        
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid() )
+        {
+            if(!$article->getId())
+            {
+                $article->setCreatedAt(new \DateTime());
+            }
+            $manager->persist($article);
+            $manager->flush();
+            
+            return $this->redirectToRoute("blog_display_article", [
+                'id'=> $article->getId()       
+            ]);
+        }
+        
+        
+        return $this->render( "blog/create.html.twig", [
+            "formArticle" => $form->createView(),
+            "articleExist" => $article->getId() !== null
+        ] );
     }
 
     /**
      * @Route("/blog/GET/{id}", name="blog_display_article")
      */
-    public function show( MaPremiereTable $article )
+    public function show( Article $article )
     {
         return $this->render( "blog/article.html.twig", [
                     "article" => $article
